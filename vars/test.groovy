d8b2a19be15eec1f8428f12d3b1fbd2d9264927a
Prashant200999 <160811688+Prashant200999@users.noreply.github.com>
2025-03-16 15:22:17 +0530

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,20 @@
+def call() {
+    script {
+        // Stage: Clean Workspace
+        stage('Clean Workspace') {
+            cleanWs()  
+        }
+
+        // Stage: Clone Repository
+        stage('Clone Repository') {
+            git url: 'https://github.com/OT-MICROSERVICES/employee-api.git', 
+                branch: 'main', 
+                credentialsId: 'Prashant_git'  
+        }
+
+        // Stage: Compile Code
+        stage('Compile') {
+            sh "mvn clean compile"  
+        }
+    }
+}
